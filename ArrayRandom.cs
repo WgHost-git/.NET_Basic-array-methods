82f6f6e4c1321203543339d41e9067252d0fd0a0
Konyahin Vitaliy <56167643+WgHost-git@users.noreply.github.com>
2020-01-03 10:26:16 +0300

    Add files via upload

---
+++ ArrayRandom.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRandom
+{
+    class Program
+    {
+        static int[,] GetRandomArray(int rows, int columns) // функция заполнения массива случайными числами
+        {
+            Random random = new Random();
+            var result = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    result[i, j] = random.Next(100);
+                }
+            }
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int rows = 5;
+            int columns = 5;
+
+            var myArr = GetRandomArray(rows, columns); 
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{myArr[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
